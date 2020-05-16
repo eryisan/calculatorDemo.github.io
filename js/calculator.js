79202515ded32e14188492ae10c7e0f1788249fd
eryisan <525559856@qq.com>
2020-05-17 07:57:07 +0800

    version 2.0

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,4 @@
-// 最容易做到的
+/* // 最容易做到的
 // button添加onclick事件
 // 获取元素
     var calculator   = document.querySelector("#calculator");
@@ -29,4 +29,47 @@ function multiplyHandler(){
 function divideHandler(){
     sign.innerHTML = "÷";
     resultOutput.innerHTML = formerInput.value / laterInput.value;
+} */
+
+
+// 第一次改进---结构和行为分离
+var calculator   = document.querySelector("#calculator");
+var formerInput  = calculator.querySelector(".formerInput");
+var laterInput   = calculator.querySelector(".laterInput");
+var sign         = calculator.querySelector(".sign");
+var resultOutput = calculator.querySelector(".resultOutput");
+var btns         = calculator.querySelectorAll(".btn");
+
+// 绑定事件
+// +
+btns[0].onclick = addHandler;
+// -
+btns[1].onclick = subtractHandler;
+// ×
+btns[2].onclick = multiplyHandler;
+// ÷
+btns[3].onclick = divideHandler;
+
+// 加
+function addHandler(){
+sign.innerHTML = "+";
+resultOutput.innerHTML = +formerInput.value + +laterInput.value;
+}
+
+// 减
+function subtractHandler(){
+sign.innerHTML = "-";
+resultOutput.innerHTML = formerInput.value - laterInput.value;
+}
+
+// 乘
+function multiplyHandler(){
+sign.innerHTML = "×";
+resultOutput.innerHTML = formerInput.value * laterInput.value;
+}
+
+// 除
+function divideHandler(){
+sign.innerHTML = "÷";
+resultOutput.innerHTML = formerInput.value / laterInput.value;
 }
