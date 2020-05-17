67f4808832062bf89459a0d5b5c10964e210dadb
eryisan <525559856@qq.com>
2020-05-17 14:47:03 +0800

    version 7.0

--- js/calculator.js
+++ js/calculator.js
@@ -320,100 +320,100 @@ function divideHandler(){
 
 
 
-// --------------------------------------------------------------------------------------------
-// 第五次改进---开放与封闭原则 Open Closed principle   OCP原则
-var wrapElem = document.querySelector("#calculator");
-var calculatorElem = {
-    formerInput   : wrapElem.querySelector(".formerInput"),
-    laterInput    : wrapElem.querySelector(".laterInput"),
-    sign          : wrapElem.querySelector(".sign"),
-    resultOutput  : wrapElem.querySelector(".resultOutput"),
-    btns          : wrapElem.querySelectorAll(".btn")
-}
+// // --------------------------------------------------------------------------------------------
+// // 第五次改进---开放与封闭原则 Open Closed principle   OCP原则
+// var wrapElem = document.querySelector("#calculator");
+// var calculatorElem = {
+//     formerInput   : wrapElem.querySelector(".formerInput"),
+//     laterInput    : wrapElem.querySelector(".laterInput"),
+//     sign          : wrapElem.querySelector(".sign"),
+//     resultOutput  : wrapElem.querySelector(".resultOutput"),
+//     btns          : wrapElem.querySelectorAll(".btn")
+// }
 
-// 遍历
-function each(array,fn){
-    for(var i=0;i<array.length;i++){
-        fn(i,array[i]);
-    }
-}
+// // 遍历
+// function each(array,fn){
+//     for(var i=0;i<array.length;i++){
+//         fn(i,array[i]);
+//     }
+// }
 
-// 绑定事件
-each(calculatorElem.btns,function(index,elem){
-    elem.onclick = function(){
-        updateSign(this.value);
-        outputResult(operate(this.title,calculatorElem.formerInput.value,calculatorElem.laterInput.value));
-        /* switch(this.title){
-            case "add":
-                // addHandler();
-                outputResult( operation.add(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
-                break;
-            case "subtract":
-                // subtractHandler();
-                outputResult( operation.subtract(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
-                break;
-            case "multiply":
-                // multiplyHandler();
-                outputResult( operation.multiply(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
-                break;
-            case "divide":
-                // divideHandler();
-                outputResult( operation.divide(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
-                break;
-            case "mod":
-                // divideHandler();
-                outputResult( operation.mod(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
-                break;
-        } */
-    }; 
-});
+// // 绑定事件
+// each(calculatorElem.btns,function(index,elem){
+//     elem.onclick = function(){
+//         updateSign(this.value);
+//         outputResult(operate(this.title,calculatorElem.formerInput.value,calculatorElem.laterInput.value));
+//         /* switch(this.title){
+//             case "add":
+//                 // addHandler();
+//                 outputResult( operation.add(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
+//                 break;
+//             case "subtract":
+//                 // subtractHandler();
+//                 outputResult( operation.subtract(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
+//                 break;
+//             case "multiply":
+//                 // multiplyHandler();
+//                 outputResult( operation.multiply(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
+//                 break;
+//             case "divide":
+//                 // divideHandler();
+//                 outputResult( operation.divide(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
+//                 break;
+//             case "mod":
+//                 // divideHandler();
+//                 outputResult( operation.mod(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
+//                 break;
+//         } */
+//     }; 
+// });
 
-// 更新符号
-function updateSign(symbol){
-    calculatorElem.sign.innerHTML = symbol;
-}
+// // 更新符号
+// function updateSign(symbol){
+//     calculatorElem.sign.innerHTML = symbol;
+// }
 
-//  运算
-function operate(name,num1,num2){
-    if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
-    return operation[name](num1,num2);
-}
-
-
-var operation = {
-    add : function(num1,num2){
-        return +num1 + +num2;
-    },
-    subtract : function(num1,num2){
-        return num1 - num2;
-    },
-    multiply : function(num1,num2){
-        return num1 * num2;
-    },
-    divide : function(num1,num2){
-        return num1 / num2;
-    },
-    addOperation:function(name,fn){
-        if(!this[name]){
-            this[name] = fn;
-        }
-        return this;
-    }
-}
-
-// 添加
-operation.addOperation("mod",function(num1,num2){
-    return num1 % num2;
-}).addOperation("power",function(base,power){
-    return Math.pow(base,power);
-});
+// //  运算
+// function operate(name,num1,num2){
+//     if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
+//     return operation[name](num1,num2);
+// }
 
 
+// var operation = {
+//     add : function(num1,num2){
+//         return +num1 + +num2;
+//     },
+//     subtract : function(num1,num2){
+//         return num1 - num2;
+//     },
+//     multiply : function(num1,num2){
+//         return num1 * num2;
+//     },
+//     divide : function(num1,num2){
+//         return num1 / num2;
+//     },
+//     addOperation:function(name,fn){
+//         if(!this[name]){
+//             this[name] = fn;
+//         }
+//         return this;
+//     }
+// }
 
-// 输出结果
-function outputResult(result){
-    calculatorElem.resultOutput.innerHTML = result;
-}
+// // 添加
+// operation.addOperation("mod",function(num1,num2){
+//     return num1 % num2;
+// }).addOperation("power",function(base,power){
+//     return Math.pow(base,power);
+// });
+
+
+
+// // 输出结果
+// function outputResult(result){
+//     calculatorElem.resultOutput.innerHTML = result;
+// }
 
 /* // 加
 function addHandler(){
@@ -438,3 +438,111 @@ function divideHandler(){
     updateSign("÷");
     outputResult( operation.divide(calculatorElem.formerInput.value,calculatorElem.laterInput.value) );
 } */
+
+
+
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+// 第六次改进---模块化
+
+(function(){
+    var wrapElem = document.querySelector("#calculator");
+    var calculatorElem = {
+        formerInput   : wrapElem.querySelector(".formerInput"),
+        laterInput    : wrapElem.querySelector(".laterInput"),
+        sign          : wrapElem.querySelector(".sign"),
+        resultOutput  : wrapElem.querySelector(".resultOutput"),
+        btns          : wrapElem.querySelectorAll(".btn")
+    }
+    
+    // 遍历
+    function each(array,fn){
+        for(var i=0;i<array.length;i++){
+            fn(i,array[i]);
+        }
+    }
+    
+    // 绑定事件
+    each(calculatorElem.btns,function(index,elem){
+        elem.onclick = function(){
+            updateSign(this.value);
+            outputResult(operate(this.title,calculatorElem.formerInput.value,calculatorElem.laterInput.value));
+        }; 
+    });
+    
+    // 更新符号
+    function updateSign(symbol){
+        calculatorElem.sign.innerHTML = symbol;
+    }
+    
+    //  运算
+    var operate = (function(){
+        var operation = {
+            add : function(num1,num2){
+                return +num1 + +num2;
+            },
+            subtract : function(num1,num2){
+                return num1 - num2;
+            },
+            multiply : function(num1,num2){
+                return num1 * num2;
+            },
+            divide : function(num1,num2){
+                return num1 / num2;
+            },
+            addOperation:function(name,fn){
+                if(!operation[name]){
+                    operation[name] = fn;
+                }
+                return operation;
+            }
+        };
+
+        /* function operate(name,num1,num2){
+            if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
+            return operation[name](num1,num2);
+        }
+
+        return operate; */
+        
+        /* return function(name,num1,num2){
+            if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
+            return operation[name](num1,num2);
+        }; */
+
+        /* function operate(name,num1,num2){
+            if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
+            return operation[name](num1,num2);
+        } */
+        function operate(name){
+            if(!operation[name]) throw new Error("不存在名为" + name + "的运算方法");
+            return operation[name].apply(operation,[].slice.call(arguments,1,arguments.length));
+        }
+
+        operate.addOperation = operation.addOperation;
+
+        return operate;
+        
+    })();
+    
+    
+    // 添加
+    operate.addOperation("mod",function(num1,num2){
+        return num1 % num2;
+    }).addOperation("power",function(base,power){
+        return Math.pow(base,power);
+    }).addOperation("invert",function(num){
+        return 1/num;
+    });
+    
+    
+    
+    // 输出结果
+    function outputResult(result){
+        calculatorElem.resultOutput.innerHTML = result;
+    }        
+})();
+
+
+
