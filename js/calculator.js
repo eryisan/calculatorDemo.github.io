4d50855028e9d55f743cb85d76555d1eb744d7d7
eryisan <525559856@qq.com>
2020-05-17 08:32:40 +0800

    version 3.0

--- js/calculator.js
+++ js/calculator.js
@@ -31,8 +31,8 @@ function divideHandler(){
     resultOutput.innerHTML = formerInput.value / laterInput.value;
 } */
 
-
-// 第一次改进---结构和行为分离
+// -------------------------------------------------------------------------------------------
+/* // 第一次改进---结构和行为分离
 var calculator   = document.querySelector("#calculator");
 var formerInput  = calculator.querySelector(".formerInput");
 var laterInput   = calculator.querySelector(".laterInput");
@@ -49,6 +49,69 @@ btns[1].onclick = subtractHandler;
 btns[2].onclick = multiplyHandler;
 // ÷
 btns[3].onclick = divideHandler;
+
+// 加
+function addHandler(){
+sign.innerHTML = "+";
+resultOutput.innerHTML = +formerInput.value + +laterInput.value;
+}
+
+// 减
+function subtractHandler(){
+sign.innerHTML = "-";
+resultOutput.innerHTML = formerInput.value - laterInput.value;
+}
+
+// 乘
+function multiplyHandler(){
+sign.innerHTML = "×";
+resultOutput.innerHTML = formerInput.value * laterInput.value;
+}
+
+// 除
+function divideHandler(){
+sign.innerHTML = "÷";
+resultOutput.innerHTML = formerInput.value / laterInput.value;
+} */
+
+
+// --------------------------------------------------------------------------------------------
+// 第二次改进---绑定事件使用循环
+var calculator   = document.querySelector("#calculator");
+var formerInput  = calculator.querySelector(".formerInput");
+var laterInput   = calculator.querySelector(".laterInput");
+var sign         = calculator.querySelector(".sign");
+var resultOutput = calculator.querySelector(".resultOutput");
+var btns         = calculator.querySelectorAll(".btn");
+
+// 绑定事件
+for(var i=0;i<btns.length;i++){
+    btns[i].onclick = function(){
+        switch(this.title){
+            case "add":
+                addHandler();
+                break;
+            case "subtract":
+                subtractHandler();
+                break;
+            case "multiply":
+                multiplyHandler();
+                break;
+            case "divide":
+                divideHandler();
+                break;
+        }
+    } 
+}
+
+/* // +
+btns[0].onclick = addHandler;
+// -
+btns[1].onclick = subtractHandler;
+// ×
+btns[2].onclick = multiplyHandler;
+// ÷
+btns[3].onclick = divideHandler; */
 
 // 加
 function addHandler(){
